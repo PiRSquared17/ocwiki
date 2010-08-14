62c8d9a6bec7a93cdca97f9527cfb09306489d49
ngocminh.oss <ngocminh.oss@localhost>
2010-08-15 00:33:47 +0700

    csdl

--- ocwiki.sql
+++ ocwiki.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 08, 2010 at 11:48 PM
+-- Generation Time: Aug 15, 2010 at 12:29 AM
 -- Server version: 5.1.41
 -- PHP Version: 5.3.2-1ubuntu4.2
 
@@ -16,7 +16,7 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `ocwiki_unittest`
+-- Database: `ocwiki`
 --
 
 -- --------------------------------------------------------
@@ -30,26 +30,22 @@ CREATE TABLE IF NOT EXISTS `ocwAnswer` (
   `ans_question` int(11) NOT NULL,
   `ans_content` int(11) NOT NULL,
   `ans_correct` tinyint(1) NOT NULL,
-  `ans_deleted` tinyint(1) NOT NULL DEFAULT '0',
+  `ans_status` varchar(10) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT 'NORMAL',
   `ans_version` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`ans_id`),
   KEY `ans_content` (`ans_content`),
   KEY `ans_question` (`ans_question`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data for table `ocwAnswer`
 --
 
-INSERT INTO `ocwAnswer` (`ans_id`, `ans_question`, `ans_content`, `ans_correct`, `ans_deleted`, `ans_version`) VALUES
-(1, 1, 10, 1, 0, 0),
-(2, 1, 11, 0, 0, 0),
-(3, 1, 12, 0, 0, 0),
-(4, 1, 13, 0, 0, 0),
-(5, 2, 14, 0, 0, 0),
-(6, 2, 15, 0, 0, 0),
-(7, 2, 16, 1, 0, 0),
-(8, 2, 17, 0, 0, 0);
+INSERT INTO `ocwAnswer` (`ans_id`, `ans_question`, `ans_content`, `ans_correct`, `ans_status`, `ans_version`) VALUES
+(1, 1, 2, 0, 'NORMAL', 0),
+(2, 1, 3, 0, 'NORMAL', 0),
+(3, 1, 4, 0, 'NORMAL', 0),
+(4, 1, 5, 1, 'NORMAL', 0);
 
 -- --------------------------------------------------------
 
@@ -68,14 +64,6 @@ CREATE TABLE IF NOT EXISTS `ocwArticleTopic` (
 -- Dumping data for table `ocwArticleTopic`
 --
 
-INSERT INTO `ocwArticleTopic` (`art_id`, `art_topic`) VALUES
-(1, 102),
-(4, 102),
-(5, 102),
-(6, 102),
-(7, 102),
-(2, 104),
-(3, 104);
 
 -- --------------------------------------------------------
 
@@ -115,36 +103,13 @@ CREATE TABLE IF NOT EXISTS `ocwConstraint` (
   `lc_level` int(11) DEFAULT NULL,
   `con_count` int(11) NOT NULL,
   `con_version` int(11) NOT NULL DEFAULT '0',
-  `con_deleted` tinyint(1) NOT NULL DEFAULT '0',
+  `con_status` varchar(10) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT 'NORMAL',
   PRIMARY KEY (`con_id`),
   KEY `con_section` (`con_section`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
 -- Dumping data for table `ocwConstraint`
---
-
-
--- --------------------------------------------------------
-
---
--- Table structure for table `ocwExam`
---
-
-CREATE TABLE IF NOT EXISTS `ocwExam` (
-  `exam_id` int(11) NOT NULL AUTO_INCREMENT,
-  `exam_teacher` int(11) NOT NULL,
-  `exam_test` int(11) NOT NULL,
-  `exam_date` date NOT NULL,
-  `exam_name` varchar(200) COLLATE utf8_vietnamese1_ci NOT NULL,
-  `exam_class` varchar(200) COLLATE utf8_vietnamese1_ci NOT NULL,
-  PRIMARY KEY (`exam_id`),
-  KEY `exam_teacher` (`exam_teacher`),
-  KEY `exam_test` (`exam_test`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese1_ci AUTO_INCREMENT=1 ;
-
---
--- Dumping data for table `ocwExam`
 --
 
 
@@ -183,7 +148,7 @@ CREATE TABLE IF NOT EXISTS `ocwQuestion` (
   `ques_level` int(11) NOT NULL,
   `art_author` int(11) NOT NULL,
   `art_create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `art_deleted` tinyint(1) NOT NULL DEFAULT '0',
+  `art_status` varchar(10) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT 'NORMAL',
   `art_last_change` int(11) DEFAULT NULL,
   `art_version` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`art_id`),
@@ -196,12 +161,8 @@ CREATE TABLE IF NOT EXISTS `ocwQuestion` (
 -- Dumping data for table `ocwQuestion`
 --
 
-INSERT INTO `ocwQuestion` (`art_id`, `art_content`, `ques_level`, `art_author`, `art_create_date`, `art_deleted`, `art_last_change`, `art_version`) VALUES
-(1, 3, 3, 1, '2010-08-01 14:45:03', 0, NULL, 0),
-(2, 4, 2, 1, '2010-08-01 14:45:03', 0, NULL, 0),
-(3, 4, 2, 1, '2010-08-01 14:45:03', 1, NULL, 0),
-(4, 3, 3, 1, '2010-08-01 14:45:03', 1, NULL, 0),
-(5, 3, 2, 1, '2010-08-01 14:45:03', 1, NULL, 0);
+INSERT INTO `ocwQuestion` (`art_id`, `art_content`, `ques_level`, `art_author`, `art_create_date`, `art_status`, `art_last_change`, `art_version`) VALUES
+(1, 1, 3, 1, '2010-08-14 10:05:31', 'NORMAL', NULL, 0);
 
 -- --------------------------------------------------------
 
@@ -214,18 +175,16 @@ CREATE TABLE IF NOT EXISTS `ocwSection` (
   `sect_test` int(11) NOT NULL,
   `sect_text` varchar(10000) CHARACTER SET utf8 COLLATE utf8_vietnamese1_ci DEFAULT NULL,
   `sect_order` int(11) NOT NULL,
-  `sect_deleted` tinyint(1) NOT NULL DEFAULT '0',
+  `sect_status` varchar(10) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT 'NORMAL',
   `sect_version` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`sect_id`),
   KEY `tblSection_ibfk_1` (`sect_test`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
 -- Dumping data for table `ocwSection`
 --
 
-INSERT INTO `ocwSection` (`sect_id`, `sect_test`, `sect_text`, `sect_order`, `sect_deleted`, `sect_version`) VALUES
-(1, 6, '21', 1, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -238,23 +197,17 @@ CREATE TABLE IF NOT EXISTS `ocwSectionQuestion` (
   `sq_section` int(11) NOT NULL,
   `sq_question` int(11) NOT NULL,
   `sq_mark` int(11) NOT NULL DEFAULT '1',
-  `sq_deleted` tinyint(1) NOT NULL DEFAULT '0',
+  `sq_status` varchar(10) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT 'NORMAL',
   `sq_version` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`sq_id`),
   UNIQUE KEY `uq_section_question` (`sq_section`,`sq_question`),
   KEY `sq_question` (`sq_question`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
 -- Dumping data for table `ocwSectionQuestion`
 --
 
-INSERT INTO `ocwSectionQuestion` (`sq_id`, `sq_section`, `sq_question`, `sq_mark`, `sq_deleted`, `sq_version`) VALUES
-(1, 1, 1, 1, 0, 0),
-(2, 1, 2, 1, 0, 0),
-(3, 1, 3, 1, 0, 0),
-(4, 1, 4, 1, 0, 0),
-(5, 1, 5, 1, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -273,18 +226,6 @@ CREATE TABLE IF NOT EXISTS `ocwSectionQuestionAnswer` (
 -- Dumping data for table `ocwSectionQuestionAnswer`
 --
 
-INSERT INTO `ocwSectionQuestionAnswer` (`sq_id`, `ans_id`) VALUES
-(1, 1),
-(2, 1),
-(4, 1),
-(1, 2),
-(4, 2),
-(1, 3),
-(3, 3),
-(4, 3),
-(1, 4),
-(5, 4),
-(1, 5);
 
 -- --------------------------------------------------------
 
@@ -298,7 +239,7 @@ CREATE TABLE IF NOT EXISTS `ocwSectionStructure` (
   `sstr_content` int(11) DEFAULT NULL,
   `sstr_order` int(11) NOT NULL,
   `sstr_version` int(11) NOT NULL DEFAULT '0',
-  `sstr_deleted` tinyint(1) NOT NULL DEFAULT '0',
+  `sstr_status` varchar(10) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT 'NORMAL',
   PRIMARY KEY (`sstr_id`),
   KEY `sstr_test` (`sstr_test`),
   KEY `sstr_content` (`sstr_content`)
@@ -323,7 +264,7 @@ CREATE TABLE IF NOT EXISTS `ocwTest` (
   `art_author` int(11) NOT NULL,
   `test_type` char(5) CHARACTER SET ascii COLLATE ascii_bin NOT NULL,
   `test_time` int(11) NOT NULL COMMENT 'Thời gian làm bài, tính bằng phút',
-  `art_deleted` tinyint(1) NOT NULL DEFAULT '0',
+  `art_status` varchar(10) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT 'NORMAL',
   `art_last_change` int(11) DEFAULT NULL,
   `art_version` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`art_id`),
@@ -337,9 +278,6 @@ CREATE TABLE IF NOT EXISTS `ocwTest` (
 -- Dumping data for table `ocwTest`
 --
 
-INSERT INTO `ocwTest` (`art_id`, `test_name`, `art_content`, `art_create_date`, `art_author`, `test_type`, `test_time`, `art_deleted`, `art_last_change`, `art_version`) VALUES
-(6, 'Tiếng Anh khối D 2010', 20, '2010-08-01 14:45:04', 1, 'tngh', 90, 0, NULL, 0),
-(7, 'Tiếng Anh khối D 2010 (dự bị 1)', 20, '2010-08-01 14:45:04', 1, 'tngh', 120, 1, NULL, 0);
 
 -- --------------------------------------------------------
 
@@ -355,7 +293,7 @@ CREATE TABLE IF NOT EXISTS `ocwTestStructure` (
   `art_create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `tstr_time` int(11) NOT NULL,
   `tstr_type` char(5) CHARACTER SET ascii COLLATE ascii_bin NOT NULL,
-  `art_deleted` tinyint(1) NOT NULL DEFAULT '0',
+  `art_status` varchar(10) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT 'NORMAL',
   `art_version` int(11) NOT NULL DEFAULT '0',
   `art_last_change` int(11) DEFAULT NULL,
   PRIMARY KEY (`art_id`),
@@ -380,29 +318,18 @@ CREATE TABLE IF NOT EXISTS `ocwText` (
   `txt_id` int(11) NOT NULL AUTO_INCREMENT,
   `txt_text` mediumtext COLLATE utf8_vietnamese1_ci NOT NULL,
   PRIMARY KEY (`txt_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese1_ci AUTO_INCREMENT=22 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese1_ci AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `ocwText`
 --
 
 INSERT INTO `ocwText` (`txt_id`, `txt_text`) VALUES
-(1, 'Đây là văn bản thử nghiệm'),
-(2, 'Việt Nam vô địch!!'),
-(3, 'Neil Armstrong was the first man ______ on the moon.'),
-(4, 'Ai là người đầu tiên đặt chân lên mặt trăng?'),
-(5, 'Cấu trúc đề thi đại học 2010'),
-(6, 'Phần I - Nghe'),
-(10, 'to walk'),
-(11, 'walking'),
-(12, 'walked'),
-(13, 'has walked'),
-(14, 'Đặng Tiểu Bình'),
-(15, 'Geogre W.Bush'),
-(16, 'Neil Amstrong'),
-(17, 'Lans Amstrong'),
-(20, 'Good luck!'),
-(21, 'Part I - Listening');
+(1, 'jafla;dsfaf\r\na \r\n\r\nfas\r\nf'),
+(2, 'lkjflkdsfafasdf'),
+(3, 'lkjasfkljdsf'),
+(4, 'kjhkjhlj;l'),
+(5, 'kjhkjhlj;lkhhlk');
 
 -- --------------------------------------------------------
 
@@ -418,7 +345,7 @@ CREATE TABLE IF NOT EXISTS `ocwTopic` (
   `art_author` int(11) NOT NULL,
   `art_create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `art_last_change` int(11) DEFAULT NULL,
-  `art_deleted` tinyint(1) NOT NULL DEFAULT '0',
+  `art_status` varchar(10) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT 'NORMAL',
   `art_version` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`art_id`),
   UNIQUE KEY `top_name` (`top_name`),
@@ -429,12 +356,6 @@ CREATE TABLE IF NOT EXISTS `ocwTopic` (
 -- Dumping data for table `ocwTopic`
 --
 
-INSERT INTO `ocwTopic` (`art_id`, `top_name`, `top_parent`, `art_content`, `art_author`, `art_create_date`, `art_last_change`, `art_deleted`, `art_version`) VALUES
-(101, 'Ngoại ngữ', 105, NULL, 1, '2010-08-01 14:45:02', NULL, 0, 0),
-(102, 'Tiếng Anh', 101, NULL, 1, '2010-08-01 14:45:02', NULL, 0, 0),
-(103, 'Tiếng Pháp', 101, NULL, 1, '2010-08-01 14:45:02', NULL, 0, 0),
-(104, 'Khoa học', NULL, NULL, 1, '2010-08-01 14:45:02', NULL, 0, 0),
-(105, 'Xã hội', NULL, NULL, 1, '2010-08-01 14:45:02', NULL, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -481,7 +402,7 @@ CREATE TABLE IF NOT EXISTS `ocwUser` (
 --
 
 INSERT INTO `ocwUser` (`user_id`, `user_name`, `user_fullname`, `user_pass`, `user_email`, `user_group`, `user_avatar`, `user_warning`, `user_blocked`, `user_register_date`) VALUES
-(1, 'cumeo89', 'Lê Ngọc Minh', '1234', 'cumeo89@gmail.com', 'admin', NULL, NULL, 0, '2010-08-01 14:45:02');
+(1, 'admin', 'abc', '1234', 'admin@ocwiki.org', 'teacher', NULL, NULL, 0, '2010-08-14 09:45:50');
 
 --
 -- Constraints for dumped tables
@@ -512,13 +433,6 @@ ALTER TABLE `ocwChange`
 --
 ALTER TABLE `ocwConstraint`
   ADD CONSTRAINT `ocwConstraint_ibfk_1` FOREIGN KEY (`con_section`) REFERENCES `ocwSectionStructure` (`sstr_id`);
-
---
--- Constraints for table `ocwExam`
---
-ALTER TABLE `ocwExam`
-  ADD CONSTRAINT `ocwExam_ibfk_1` FOREIGN KEY (`exam_teacher`) REFERENCES `ocwUser` (`user_id`),
-  ADD CONSTRAINT `ocwExam_ibfk_4` FOREIGN KEY (`exam_test`) REFERENCES `ocwTest` (`art_id`);
 
 --
 -- Constraints for table `ocwHistory`
