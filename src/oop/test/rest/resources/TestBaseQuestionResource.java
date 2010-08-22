f9674ecca3305f6bf0847eb4f12eb5cf5f67de4f
sdsbirdman <sdsbirdman@gmail.com>
2010-08-23 01:30:24 +0700

    DungTV -------------- Tao question service
    (Chua test... di ngu luon vi met)

---
+++ src/oop/test/rest/resources/TestBaseQuestionResource.java
@@ -0,0 +1,22 @@
+package oop.test.rest.resources;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestBaseQuestionResource {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAddAndDeleteQuestion(){
+		
+	}
+}
