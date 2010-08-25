ba1a549e6e1a9b11b9dd5bee29fd4e282317ef55
ngocminh.oss <ngocminh.oss@localhost>
2010-08-25 22:21:43 +0700

    l?c phi?n b?n theo ng?y th?ng

--- src/oop/db/dao/RevisionDAO.java
+++ src/oop/db/dao/RevisionDAO.java
@@ -1,5 +1,6 @@
 package oop.db.dao;
 
+import java.util.Date;
 import java.util.List;
 
 import oop.data.Article;
@@ -23,6 +24,18 @@ public class RevisionDAO {
 		Query query = session
 				.createQuery("from Revision where resource.id=:resId");
 		query.setLong("resId", resourceId);
+		query.setFirstResult(start);
+		query.setMaxResults(size);
+		return query.list();
+	}
+
+	public static List<Revision<Article>> fetchByResourceBefore(
+			long resourceId, Date timestamp, int start, int size) {
+		Session session = HibernateUtil.getSession();
+		String hql = "from Revision where resource.id=:resId and timestamp<=:ts";
+		Query query = session.createQuery(hql);
+		query.setLong("resId", resourceId);
+		query.setDate("ts", timestamp);
 		query.setFirstResult(start);
 		query.setMaxResults(size);
 		return query.list();
