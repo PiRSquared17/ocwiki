6dd35eaaf6c183faf2d6f133c9d004ce349e8f2e
ngocminh.oss <ngocminh.oss@localhost>
2010-09-04 22:54:50 +0700

    UserResourceTest.testUpdate

--- test/src/oop/test/rest/servletunit/UserResouceTest.java
+++ test/src/oop/test/rest/servletunit/UserResouceTest.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import oop.conf.Config;
 
 import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.node.ObjectNode;
 import org.junit.Assert;
 import org.junit.Test;
 import org.xml.sax.SAXException;
@@ -29,6 +30,25 @@ public class UserResouceTest extends ResourceTest {
 		Assert.assertEquals("admin", result.get("name").getTextValue());
 		Assert.assertEquals(false, result.get("blocked").getBooleanValue());
 		getServletRunner().shutDown();
+	}
+	
+	@Test
+	public void testUpdate() throws IOException, SAXException {
+		String blockExpiredDate = "2010-10-25T00:47:13+07:00";
+
+		ServletUnitClient sc = getServletRunner().newClient();
+		String url = Config.get().getRestPath() + "/users/1";
+		WebResponse response = sc.getResponse(url);
+		ObjectNode root = (ObjectNode) parseJSON(response).get("result");
+		
+		root.put("blocked", true);
+		root.put("blockExpiredDate", blockExpiredDate);
+		JsonBodyPostWebRequest request = new JsonBodyPostWebRequest(url, root.toString());
+		response = sc.getResponse(request);
+		
+		root = (ObjectNode) parseJSON(response).get("result");
+		Assert.assertEquals("true", root.get("blocked").getValueAsText());
+		Assert.assertEquals(blockExpiredDate, root.get("blockExpiredDate").getValueAsText());
 	}
 
 }
