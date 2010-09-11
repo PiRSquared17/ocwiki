948986b5bee31bffe652ce1abfa6c92ec07e5209
ngocminh.oss <ngocminh.oss@localhost>
2010-09-11 22:59:25 +0700

    "?c s? d?ng trong ?? thi" module

--- src/oop/db/dao/TestDAO.java
+++ src/oop/db/dao/TestDAO.java
@@ -102,13 +102,13 @@ public class TestDAO {
 	public static List<Resource<Test>> fetchByContainedQuestion(
 			long baseQuestionResourceId, int start, int length) {
 		Session session = HibernateUtil.getSession();
-		String hql = "from Resource where article in " +
-				"(from Test as test " +
+		String hql = "from Resource where article.id in " +
+				"(select test.id from Test as test " +
 					"inner join test.sections as section " +
 					"inner join section.questions as question " +
 				"with question.baseResource.id = :qid)";
 		Query query = session.createQuery(hql);
-		query.setLong(":qid", baseQuestionResourceId);
+		query.setLong("qid", baseQuestionResourceId);
 		query.setFirstResult(start);
 		query.setMaxResults(length);
 		return query.list();
