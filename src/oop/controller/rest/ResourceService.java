a6cce485d8c12f25239f80fde1ea038e42b4ed2a
phtdce <phtdce@localhost>
2010-09-12 11:27:50 +0700

    khoa - mo bai` viet

---
+++ src/oop/controller/rest/ResourceService.java
@@ -0,0 +1,42 @@
+package oop.controller.rest;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import oop.controller.rest.util.ListResult;
+import oop.controller.rest.util.ObjectResult;
+import oop.data.Article;
+import oop.data.Resource;
+import oop.data.User;
+import oop.db.dao.ResourceDAO;
+
+
+@Path("/resource")
+public class ResourceService extends AbstractResource{
+
+	
+	@GET
+	@Path("/{id: \\d+}")
+	public ObjectResult<Resource<Article>> get(@PathParam("id") long id) {
+		Resource<Article> resource = ResourceDAO.fetchById(id);
+		assertResourceFound(resource);
+		return new ObjectResult<Resource<Article>>(resource);
+	}
+	
+	@POST
+	@Path("/{id: \\d+}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public ObjectResult<Resource<Article>> update(@PathParam("id") long id, Resource<Article> data){
+		Resource<Article> resource = ResourceDAO.fetchById(id);
+		this.assertResourceFound(resource);
+		resource.setAccessibility(data.getAccessibility());
+		// ResourceDAO.persist(resource); cai nay` ko co a` ?
+		return new ObjectResult<Resource<Article>>(resource);
+	}
+}
