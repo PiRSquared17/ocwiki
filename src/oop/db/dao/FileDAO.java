452da0f4ab36103b86a0fb28b7a4061b963ef80c
phtdce <phtdce@localhost>
2010-09-26 22:12:20 +0700

    upload file(DAO)

---
+++ src/oop/db/dao/FileDAO.java
@@ -0,0 +1,31 @@
+package oop.db.dao;
+
+import oop.data.File;
+import oop.persistence.HibernateUtil;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class FileDAO {
+
+	public static File fetchById(long id) throws Exception {
+		Session session = HibernateUtil.getSession();
+		return (File) session.get(File.class, id);
+	}
+	
+	public static void persist(File file) {
+		Session session = HibernateUtil.getSession();
+		Transaction tx = null;
+		try {
+			tx = session.beginTransaction();
+			session.saveOrUpdate(file);
+			tx.commit();
+		} catch (HibernateException ex) {
+			if (tx != null) {
+				tx.rollback();
+				session.close();
+			}
+			throw ex;
+		}
+	}
+}
