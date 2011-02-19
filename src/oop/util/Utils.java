a22db783463473c5ea5c3d8c568dfe517074235a
cumeo89 <cumeo89@localhost>
2011-02-19 18:06:54 +0700

    pretty url continued

--- src/oop/util/Utils.java
+++ src/oop/util/Utils.java
@@ -160,41 +160,76 @@ public class Utils {
 		}
 		return null;
 	}
-	
-	private static char[] ORIGINAL = { ' ', 'À', 'Á', 'Â', 'Ã', 'È', 'É', 'Ê',
-			'Ì', 'Í', 'Ò', 'Ó', 'Ô', 'Õ', 'Ù', 'Ú', 'à', 'á', 'â', 'ã', 'è',
-			'é', 'ê', 'ì', 'í', 'ò', 'ó', 'ô', 'õ', 'ù', 'ú', 'Ă', 'ă', 'Đ',
-			'đ', 'Ĩ', 'ĩ', 'Ũ', 'ũ', 'Ơ', 'ơ', 'Ư', 'ư', 'Ạ', 'ạ', 'Ả', 'ả',
-			'Ấ', 'ấ', 'Ầ', 'ầ', 'Ẩ', 'ẩ', 'Ẫ', 'ẫ', 'Ậ', 'ậ', 'Ắ', 'ắ', 'Ằ',
-			'ằ', 'Ẳ', 'ẳ', 'Ẵ', 'ẵ', 'Ặ', 'ặ', 'Ẹ', 'ẹ', 'Ẻ', 'ẻ', 'Ẽ', 'ẽ',
-			'Ế', 'ế', 'Ề', 'ề', 'Ể', 'ể', 'Ễ', 'ễ', 'Ệ', 'ệ', 'Ỉ', 'ỉ', 'Ị',
-			'ị', 'Ọ', 'ọ', 'Ỏ', 'ỏ', 'Ố', 'ố', 'Ồ', 'ồ', 'Ổ', 'ổ', 'Ỗ', 'ỗ',
-			'Ộ', 'ộ', 'Ớ', 'ớ', 'Ờ', 'ờ', 'Ở', 'ở', 'Ỡ', 'ỡ', 'Ợ', 'ợ', 'Ụ',
-			'ụ', 'Ủ', 'ủ', 'Ứ', 'ứ', 'Ừ', 'ừ', 'Ử', 'ử', 'Ữ', 'ữ', 'Ự', 'ự' };
-	
-	private static char[] CONVERTED = { '_', 'A', 'A', 'A', 'A', 'E', 'E', 'E',
-			'I', 'I', 'O', 'O', 'O', 'O', 'U', 'U', 'a', 'a', 'a', 'a', 'e',
-			'e', 'e', 'i', 'i', 'o', 'o', 'o', 'o', 'u', 'u', 'A', 'a', 'D',
-			'd', 'I', 'i', 'U', 'u', 'O', 'o', 'U', 'u', 'A', 'a', 'A', 'a',
+
+	private static char[] ORIGINAL = { ' ', '!', '"', '#', '$', '%', '*', '+',
+			',', ':', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '`', '|',
+			'~', 'À', 'Á', 'Â', 'Ã', 'È', 'É', 'Ê', 'Ì', 'Í', 'Ò', 'Ó', 'Ô',
+			'Õ', 'Ù', 'Ú', 'Ý', 'à', 'á', 'â', 'ã', 'è', 'é', 'ê', 'ì', 'í',
+			'ò', 'ó', 'ô', 'õ', 'ù', 'ú', 'ý', 'Ă', 'ă', 'Đ', 'đ', 'Ĩ', 'ĩ',
+			'Ũ', 'ũ', 'Ơ', 'ơ', 'Ư', 'ư', 'Ạ', 'ạ', 'Ả', 'ả', 'Ấ', 'ấ', 'Ầ',
+			'ầ', 'Ẩ', 'ẩ', 'Ẫ', 'ẫ', 'Ậ', 'ậ', 'Ắ', 'ắ', 'Ằ', 'ằ', 'Ẳ', 'ẳ',
+			'Ẵ', 'ẵ', 'Ặ', 'ặ', 'Ẹ', 'ẹ', 'Ẻ', 'ẻ', 'Ẽ', 'ẽ', 'Ế', 'ế', 'Ề',
+			'ề', 'Ể', 'ể', 'Ễ', 'ễ', 'Ệ', 'ệ', 'Ỉ', 'ỉ', 'Ị', 'ị', 'Ọ', 'ọ',
+			'Ỏ', 'ỏ', 'Ố', 'ố', 'Ồ', 'ồ', 'Ổ', 'ổ', 'Ỗ', 'ỗ', 'Ộ', 'ộ', 'Ớ',
+			'ớ', 'Ờ', 'ờ', 'Ở', 'ở', 'Ỡ', 'ỡ', 'Ợ', 'ợ', 'Ụ', 'ụ', 'Ủ', 'ủ',
+			'Ứ', 'ứ', 'Ừ', 'ừ', 'Ử', 'ử', 'Ữ', 'ữ', 'Ự', 'ự', };
+
+	private static char[] CONVERTED = { '_', '\0', '\0', '\0', '\0', '\0',
+			'\0', '_', '\0', '_', '\0', '\0', '\0', '\0', '\0', '\0', '_',
+			'\0', '\0', '\0', '\0', '\0', 'A', 'A', 'A', 'A', 'E', 'E', 'E',
+			'I', 'I', 'O', 'O', 'O', 'O', 'U', 'U', 'Y', 'a', 'a', 'a', 'a',
+			'e', 'e', 'e', 'i', 'i', 'o', 'o', 'o', 'o', 'u', 'u', 'y', 'A',
+			'a', 'D', 'd', 'I', 'i', 'U', 'u', 'O', 'o', 'U', 'u', 'A', 'a',
 			'A', 'a', 'A', 'a', 'A', 'a', 'A', 'a', 'A', 'a', 'A', 'a', 'A',
-			'a', 'A', 'a', 'A', 'a', 'A', 'a', 'E', 'e', 'E', 'e', 'E', 'e',
-			'E', 'e', 'E', 'e', 'E', 'e', 'E', 'e', 'E', 'e', 'I', 'i', 'I',
-			'i', 'O', 'o', 'O', 'o', 'O', 'o', 'O', 'o', 'O', 'o', 'O', 'o',
-			'O', 'o', 'O', 'o', 'O', 'o', 'O', 'o', 'O', 'o', 'O', 'o', 'U',
-			'u', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u' };
-	
+			'a', 'A', 'a', 'A', 'a', 'A', 'a', 'A', 'a', 'E', 'e', 'E', 'e',
+			'E', 'e', 'E', 'e', 'E', 'e', 'E', 'e', 'E', 'e', 'E', 'e', 'I',
+			'i', 'I', 'i', 'O', 'o', 'O', 'o', 'O', 'o', 'O', 'o', 'O', 'o',
+			'O', 'o', 'O', 'o', 'O', 'o', 'O', 'o', 'O', 'o', 'O', 'o', 'O',
+			'o', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u',
+			'U', 'u', };
+
+	/**
+	 * Convert a string (article name, user name,...) to URL-friendly version.
+	 * That means:
+	 * <ul>
+	 * <li>No ASCII Control characters</li>
+	 * <li>No Vietnamese accent</li>
+	 * <li>Less than or equal to 236 in length: to be used as file name,
+	 * recommended path length is <a
+	 * href="http://msdn.microsoft.com/en-us/library/aa365247.aspx#maxpath">
+	 * 260</a> in Windows, minus 4 of ".html" thing, minus 19 of maximum length
+	 * of id, minus 1 of a dash.</li>
+	 * <li>No trailing slashes.</li>
+	 * <li>No URL- and file system- special characters: less than, greater than,
+	 * double quote, pipe, asterisk, space, colon, comma, exclamation, question
+	 * mark</li>
+	 * </ul>
+	 * 
+	 * @param s
+	 * @return
+	 */
 	public static String toUrlFriendly(String s) {
-		char[] sb = new char[Math.min(s.length(), 255)];
-		for (int i = 0; i < sb.length; i++) {
+		int length = Math.min(s.length(), 236);
+		// skip trailing slashes
+		while (s.charAt(length - 1) == '/' || s.charAt(length - 1) == '\\') {
+			length--;
+		}
+		int n = 0;
+		char[] buffer = new char[length];
+		for (int i = 0; i < length; i++) {
 			char ch = s.charAt(i);
 			int index = Arrays.binarySearch(ORIGINAL, ch);
 			if (index >= 0) {
-				sb[i] = CONVERTED[index];
+				buffer[n++] = CONVERTED[index];
 			} else {
-				sb[i] = ch;
+				buffer[n++] = ch;
+			}
+			// skip not printable characters
+			if (buffer[n-1] >= 0 && buffer[n-1] <= 31) {
+				n--;
 			}
 		}
-		return String.valueOf(sb);
+		return String.valueOf(buffer, 0, n);
 	}
 	
 }
