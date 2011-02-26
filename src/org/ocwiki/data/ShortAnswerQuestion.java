fea17ec8cf7f80972732dccce2209b3b495c5696
cumeo89 <cumeo89@localhost>
2011-02-26 21:14:01 +0700

    + question types

---
+++ src/org/ocwiki/data/ShortAnswerQuestion.java
@@ -0,0 +1,17 @@
+package org.ocwiki.data;
+
+/**
+ * These questions ask test taker to enter a short answer. The answer
+ * may be in form of a few words or a number. 
+ * @author cumeo89
+ * TODO
+ */
+public class ShortAnswerQuestion extends Question {
+
+	@Override
+	public Article copy() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
