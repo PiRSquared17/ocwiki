a4b2d3cb3d56551c9e1ca96be6e9ef9fdd101515
cumeo89 <cumeo89@localhost>
2011-02-26 21:14:01 +0700

    + question types

---
+++ src/org/ocwiki/data/BlankFillingQuestion.java
@@ -0,0 +1,16 @@
+package org.ocwiki.data;
+
+/**
+ * Questions that ask test taker to fill in one or more blanks. 
+ * @author cumeo89
+ * TODO
+ */
+public class BlankFillingQuestion extends Question {
+
+	@Override
+	public Article copy() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/org/ocwiki/data/MatchingQuestion.java
@@ -0,0 +1,17 @@
+package org.ocwiki.data;
+
+/**
+ * These questions require student to match each item in a column to an
+ * item in another column.
+ * @author cumeo89
+ * TODO
+ */
+public class MatchingQuestion extends Question {
+
+	@Override
+	public Article copy() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
