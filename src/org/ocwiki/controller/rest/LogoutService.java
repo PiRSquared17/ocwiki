8a761e7eb29012358025e762732de3404d3422dd
ngocminh.oss <ngocminh.oss@localhost>
2011-03-26 13:35:29 +0700

    logout service

---
+++ src/org/ocwiki/controller/rest/LogoutService.java
@@ -0,0 +1,17 @@
+package org.ocwiki.controller.rest;
+
+import javax.ws.rs.Path;
+
+import org.ocwiki.util.SessionUtils;
+
+@Path(LogoutService.PATH)
+public class LogoutService extends AbstractResource {
+
+	public static final String PATH = "/logout";
+
+	@Path("/")
+	public void logout() {
+		SessionUtils.logout(getSession());
+	}
+	
+}
